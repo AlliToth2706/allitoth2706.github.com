9e0fbc800cf4cfdff5663038c264b1c4e3ffa4be
Alli <alli.toth@outlook.com>
2024-07-14 12:21:33 +1000

    Move social site to its own repository

--- src/components/social/Components/Loading.jsx
+++
@@ -1,8 +0,0 @@
-import { Spinner } from '@chakra-ui/react';
-
-//loading symbol
-const Loading = ({ bool, children }) => {
-    return bool ? children : <Spinner size='xl' />;
-};
-
-export default Loading;

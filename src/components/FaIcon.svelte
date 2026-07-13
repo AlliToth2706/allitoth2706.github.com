a0df320d6b31103b2253ddf5f307965ce77c3ca2
Alli <alli.toth@outlook.com>
2026-07-13 14:58:43 +1000

    Start on revamping website

---
+++ src/components/FaIcon.svelte
@@ -0,0 +1,45 @@
+<script>
+    import { library } from "@fortawesome/fontawesome-svg-core";
+    import { fab } from "@fortawesome/free-brands-svg-icons";
+    import { FontAwesomeIcon } from "@fortawesome/svelte-fontawesome";
+    import "@fortawesome/fontawesome-free/css/all.min.css";
+
+    export let icon;
+
+    library.add(fab);
+</script>
+
+<!-- <i
+    class={`fa-brands fa-${icon}`}
+    data-popover-target="popover-bottom"
+    data-popover-placement="bottom"
+    size="sm"
+></i> -->
+<!-- <FontAwesomeIcon
+    icon={["fa-brands", icon]}
+    size="sm"
+    data-popover-target="popover-bottom"
+    data-popover-placement="bottom"
+    type="button"
+/> -->
+<button
+    type="button"
+    data-popover-target="popover-bottom"
+    data-popover-placement="bottom">test</button
+>
+<div
+    data-popover
+    id="popover-bottom"
+    role="tooltip"
+    class="absolute z-10 invisible inline-block w-64 text-sm text-body transition-opacity duration-300 bg-neutral-primary-soft border border-default rounded-base shadow-xs opacity-0"
+>
+    <div
+        class="px-3 py-2 bg-neutral-tertiary border-b border-default rounded-t-base"
+    >
+        <h3 class="font-medium text-heading">Popover bottom</h3>
+    </div>
+    <div class="px-3 py-2">
+        <p>And here's some amazing content. It's very engaging. Right?</p>
+    </div>
+    <div data-popper-arrow></div>
+</div>
--- src/components/Typewriter.svelte
+++ src/components/Typewriter.svelte
@@ -1,30 +1,90 @@
 <!-- Adapted from https://youtu.be/w1nhwUGsG6M by Kevin Powell -->
 
 <script>
+    import { onMount } from "svelte";
+
     export let text;
-    $: document.querySelector(':root').style.setProperty('--typewriterCharacters', text.length);
+    export let devMode;
+
+    let typingSpeed = 0.25;
+    let typewriterSpeed = typingSpeed * text.length;
+
+    // let indexes = [];
+    // let stringToFind = ", ";
+    // let testString = text;
+    // while (testString !== "") {
+    //     let t = testString.indexOf(stringToFind);
+    //     if (t === -1) {
+    //         testString = "";
+    //     } else {
+    //         indexes.push(t);
+    //         testString = testString.slice(t + stringToFind.length);
+    //     }
+    // }
+    // let percentages = indexes.map((e) => {
+    //     let n = e / text.length;
+    //     return n.toPrecision(3) * 100;
+    // });
+    // let pauseAnim = false;
+
+    // onMount(() => {
+    //     let typewriter = document.querySelector("#typewriter::before");
+    //     // while (true) {
+    //     //     console.log(left);
+    //     // }
+    //     (function move() {
+    //         // var character=document.getElementById("character");
+    //         let left = typewriter.style;
+    //         console.log(left);
+    //         setTimeout(move, 1e3);
+    //     })();
+    // });
+    const toMakeVisible = ["navbar", "about", "skills", "projects", "contact"];
+
+    if (devMode) {
+        toMakeVisible.forEach((item) => {
+            document.getElementById(item).removeAttribute("data-invisible");
+        });
+        document.getElementById("subheader").removeAttribute("data-hidden");
+    } else {
+        const onAnimationEnd = (e) => {
+            toMakeVisible.forEach((item) => {
+                document.getElementById(item).removeAttribute("data-invisible");
+            });
+            const subh = document.getElementById("subheader");
+            subh.removeAttribute("data-hidden");
+            subh.classList.add("animate__fadeInUp");
+        };
+        onMount(() => {
+            const el = document.getElementById("typewriter");
+            el.addEventListener("animationend", onAnimationEnd);
+        });
+    }
 </script>
 
 <span>
-    <h1 class="leading-normal relative w-max p-1 font-bold h-fit my-20 before:bg-zinc-900 after:w-3 after:bg-white">
+    <h1
+        class="leading-normal relative w-max p-1 font-bold h-fit my-20 before:bg-zinc-900 after:w-3 after:bg-white"
+        style="--typingSpeed: {typingSpeed}s; 
+            --typewriterSpeed: {typewriterSpeed}s; 
+            --typewriterCharacters: {text.length};
+            --animps: running;"
+        id="typewriter"
+    >
+        <!-- id="typewriter"  --animps: {pauseAnim ? 'paused' : 'running'};  -->
         {text}
     </h1>
 </span>
 
 <style>
-    :root {
-        --typewriterSpeed: 6s;
-        --typewriterCharacters: 13;
-    }
-
     h1 {
         font-size: clamp(1rem, 6vw + 1rem, 6rem);
-        font-family: 'Source Code Pro', monospace;
+        font-family: "Source Code Pro", monospace;
     }
 
     h1::before,
     h1::after {
-        content: '';
+        content: "";
         position: absolute;
         top: 0;
         right: 0;
@@ -33,12 +93,15 @@
     }
 
     h1::before {
-        animation: typewriter var(--typewriterSpeed) steps(var(--typewriterCharacters)) 1s forwards;
+        animation: typewriter var(--typewriterSpeed)
+            steps(var(--typewriterCharacters)) var(--typingSpeed) forwards
+            var(--animps);
     }
 
     h1::after {
         animation:
-            typewriter var(--typewriterSpeed) steps(var(--typewriterCharacters)) 1s forwards,
+            typewriter var(--typewriterSpeed) steps(var(--typewriterCharacters))
+                var(--typingSpeed) forwards var(--animps),
             blink 750ms steps(var(--typewriterCharacters)) infinite;
     }
 
